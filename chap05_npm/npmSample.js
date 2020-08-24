5021e79e3d44c10a71c9d0f674dd7789e7e9ad46
hellojohnwoo <hellojohnwoo@gmail.com>
2020-08-24 16:23:52 +0900

    0824 Npm quick

---
+++ chap05_npm/npmSample.js
@@ -0,0 +1,23 @@
+// npm uninstall [package name]    // npm rm [package name]
+//     search    
+//     info 
+
+// npm outdated
+
+
+// npm adduser [userid]
+
+// npm login
+
+// npm whoami  
+
+// npm logout
+
+
+// npm version [version number]
+
+
+// npm deprecate [package name][version number] [message] 
+// npm publish 
+// npm unpublish [package name] --force // in 72hours
+// // npm info [package name] --force
